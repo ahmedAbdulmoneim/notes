40b490422c2615ce82800c328c6a3816fda9b233
Ahmed <3lbashmohands@gmail.com>
2023-12-14 14:40:23 +0200

    change them

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/views/notesView.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: NotesView(),
     );
   }
 }
