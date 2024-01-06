d991b759cf7da3346a71fad19728d35d401da544
Ahmed <3lbashmohands@gmail.com>
2024-01-07 00:54:50 +0200

    add init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:notes/views/notesView.dart';
 
-void main() {
+void main() async{
+
+  await Hive.initFlutter();
+  await Hive.openBox('notes box');
   runApp(const MyApp());
 }
 
